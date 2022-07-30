5e4f1e24079f6c2251f2e3a1f2772582bb044bed
Matheus Gomes <matheusgomesj7@gmail.com>
2022-07-29 22:14:22 -0300

    added creation of users inside firestore through google popup auth

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,12 @@ import {
   signInWithPopup,
   GoogleAuthProvider
 } from 'firebase/auth';
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBa3jKE3zETrOR2T8gnsRCeRdpPUGqhs5I",
@@ -22,7 +28,27 @@ provider.setCustomParameters({
   prompt: 'select_account',
 });
 
-const auth = getAuth();
-const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid)
+  console.log(userDocRef)
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists())
 
-export { auth, signInWithGooglePopup };
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      });
+    } catch (err) {
+      console.log('error creating the user', err.message)
+    };
+  }
+};
