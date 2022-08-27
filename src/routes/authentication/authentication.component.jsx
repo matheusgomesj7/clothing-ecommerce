135063156b0977382cb6758b3edfc8966d0829cd
Matheus Gomes <matheusgomesj7@gmail.com>
2022-08-27 16:23:20 -0300

    test netlify

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,27 +1,24 @@
-// import { useEffect } from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
-// import { selectCurrentUser } from '../../store/user/user.selector';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectCurrentUser } from '../../store/user/user.selector';
 import SignUpForm from '../../components/sign-up-form/sign-up.component-form';
 import SignInForm from '../../components/sign-in-form/sign-in.component-form';
 import { AuthContainer } from './authentication.styles.jsx';
 
 
 const Authentication = () => {
+  const currentUser = useSelector(selectCurrentUser);
+  const navigate = useNavigate();
 
-  // const currentUser = useSelector(selectCurrentUser);
-  // const navigate = useNavigate();
-
-  // //*fixme
-
-  // useEffect(() => {
-  //   const navigateToShop = () => navigate('/shop');
-  //   setTimeout(() => {
-  //     if (currentUser) {
-  //     navigateToShop();
-  //     }
-  //   }, 500);
-  // }, [currentUser, navigate]);
+  useEffect(() => {
+    const navigateToShop = () => navigate('/shop');
+    setTimeout(() => {
+      if (currentUser) {
+      navigateToShop();
+      }
+    }, 500);
+  }, [currentUser, navigate]);
   
   return (
     <AuthContainer>
