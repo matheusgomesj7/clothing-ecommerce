7a06b7f0b2d20ac701bcad1e8462b3e1c0fceeea
Matheus Gomes <matheusgomesj7@gmail.com>
2022-10-12 20:30:31 -0300

    adding PWA feature

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { store, persistor } from './store/store';
 import { stripePromise } from './utils/stripe/stripe.utils';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -29,3 +30,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorkerRegistration.register();
